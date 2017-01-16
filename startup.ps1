c2be7b3ff748e125bb4a13ccf34e0d91d02b3093
artisticcheese <gregory.suvalian@intel.com>
2017-01-16 16:13:59 -0600

    s

--- startup.ps1
+++ startup.ps1
@@ -1,15 +1,16 @@
-$ContainerAdmin = "contadmin"
-$ContainerPassword = "A123456!"
+$ContainerAdmin = $env:ContainerAdmin
+$ContainerPassword = $env:ContainerPassword
 
-if ((Get-LocalUser -Name $ContainerAdmin) -eq $true) {
+if ((Get-LocalUser -Name $ContainerAdmin) -ne $true) {
+    Write-Verbose "Admin container user already exists, updating password"
     Set-localuser -Name $ContainerAdmin -Password (ConvertTo-SecureString  $ContainerPassword -AsPlainText -Force) -ErrorAction Stop
-
 }
 else
 {
+    Write-Verbose "Admin container does not exist, creating user"
     new-LocalUser -Name $ContainerAdmin -Password  (ConvertTo-SecureString  $ContainerPassword -AsPlainText -Force) -ErrorAction Stop
 }
-Add-LocalGroupMember -Group Administrators -Member $ContainerAdmin
+if (((Get-LocalGroupMember administrators) -match  "contadmin").Count -eq 0) {Add-LocalGroupMember -Group Administrators -Member $ContainerAdmin }
 <#
     param(
     [string] $containerAdmin = $env:ContainerAdmin,
