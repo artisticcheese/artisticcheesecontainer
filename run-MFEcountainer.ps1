a4bd1b486a5b675b8c7d4f42638d1f4ddb51e2a1
artisticcheese <artisticcheese@gmail.com>
2017-01-19 19:14:11 -0600

    b

--- run-MFEcountainer.ps1
+++ run-MFEcountainer.ps1
@@ -2,7 +2,7 @@
 $config = [Docker.DotNet.Models.Config]::new()
 #($config.Env = [System.Collections.Generic.List[string]]::new()).Add("containeradmin=$($env:ContainerAdmin)")
 #$config.Env.Add("containerrunpassword=$($env:ContainerPassword)")
-#($config.ExposedPorts  = [System.Collections.Generic.Dictionary[string,object]]::new()).Add("8080:80", $null)
+($config.ExposedPorts  = [System.Collections.Generic.Dictionary[string,object]]::new()).Add("443:80", $null)
 $hostConfig = [Docker.DotNet.Models.HostConfig]::new()
 #($hostConfig.Binds = [System.Collections.Generic.List[string]]::New()).Add('d:\docker\content:c:\logs\host')
 
@@ -10,15 +10,11 @@ $pb = new-object Docker.DotNet.Models.PortBinding
 $pb.HostPort = "443"
 $hostConfig.PortBindings = [System.Collections.Generic.Dictionary[string, System.Collections.Generic.iList[Docker.DotNet.Models.PortBinding]]]::new()
 $hostConfig.PortBindings.Add("80/tcp",[System.Collections.Generic.List[Docker.DotNet.Models.PortBinding]]::new([Docker.DotNet.Models.PortBinding[]]@($pb)))
-
-
-#($hostConfig.PortBindings  = [System.Collections.Generic.Dictionary[string,object]]::new()).Add("1433/tcp", '[{"HostIp": "", "HostPort": "1433"}]}')
 $hostConfig.NetworkMode = "nat"
 $hostconfig.CPUPercent = 33
-
 #$hostConfig.CPUQuota = 50 
-#Get-Container | remove-container -Force
+
 #Run-ContainerImage -Isolation HyperV -Name "first" -ImageIdOrName "iis:01_52_19" -HostConfiguration $hostconfig -Configuration $config -Verbose -Detach
-Run-ContainerImage -ImageIdOrName "artisticcheese/iis:latest" -Configuration $config -Detach -HostConfiguration $hostConfig -Name $pb.HostPort
+Run-ContainerImage -ImageIdOrName "microsoft/iis:latest" -Configuration $config -Detach -HostConfiguration $hostConfig -Name $pb.HostPort  
 
 
--- test/hello.ps1
+++ test/hello.ps1
@@ -1,1 +1,6 @@
-echo "fdd"
+$pb = new-object Docker.DotNet.Models.PortBinding
+$pb.HostPort = "88"
+$hostConfig = new-object Docker.DotNet.Models.HostConfig
+$hostConfig.PortBindings = [System.Collections.Generic.Dictionary[string, System.Collections.Generic.iList[Docker.DotNet.Models.PortBinding]]]::new()
+$hostConfig.PortBindings.Add("80/tcp",[System.Collections.Generic.List[Docker.DotNet.Models.PortBinding]]::new([Docker.DotNet.Models.PortBinding[]]@($pb)))
+Run-ContainerImage -HostConfiguration $hostConfig -Name microsoft/iis -Detach
